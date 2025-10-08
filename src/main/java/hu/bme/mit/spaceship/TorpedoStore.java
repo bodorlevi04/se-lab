54bf3cc46484c5d1a4d8ca7956de87a86ca6c8f0
unknown <190762@bme.hu>
2025-10-08 15:30:41 +0200

    comment

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -36,6 +36,7 @@ public class TorpedoStore {
     boolean success = false;
 
     // simulate random overheating of the launcher bay which prevents firing
+    //this added to show that generator is attribute of TorpedoStore
     double r = this.generator.nextDouble();
 
     if (r >= FAILURE_RATE) {
